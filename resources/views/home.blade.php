29c0f1c0b71185d076a95259932240510d65488a
auksaraoun <auksaraoun@hotmail.com>
2022-04-11 17:16:25 +0700

    เอา font ออก

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,12 @@
     Auksara Bingamen
 </title>
 <!--     Fonts and icons     -->
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+{{-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" /> --}}
 <!-- Nucleo Icons -->
 <link href="{{ asset('assets/frontend/css/nucleo-icons.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/frontend/css/nucleo-svg.css') }}" rel="stylesheet" />
 <!-- Font Awesome Icons -->
-<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+{{-- <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script> --}}
 <link href="{{ asset('assets/frontend/css/nucleo-svg.css') }}" rel="stylesheet" />
 <!-- CSS Files -->
 <link id="pagestyle" href="{{ asset('assets/frontend/css/soft-design-system.css') }}?v=1.0.7" rel="stylesheet" />
